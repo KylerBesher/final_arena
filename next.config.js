7f63d944d3455334f5dfa50814b6ad3046c29f1a
Kyler Besher <kyler@Kylers-MBP.home>
2025-01-03 16:28:53 -0600

    build update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack: (cfg) => {
-        cfg.module.rules.push(
-            {
-                test: /\.md$/,
-                loader: 'frontmatter-markdown-loader',
-                options: { mode: ['react-component'] }
-            }
-        )
-        return cfg
-    }
+    // pageExtensions: ["tsx"],
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push(
+            ...[
+                {
+                    test: /\.md$/,
+                    loader: 'frontmatter-markdown-loader',
+                    options: { mode: ['react-component'] }
+                },
+                {
+                    test: /\.yml$/,
+                    type: "json",
+                    use: "yaml-loader",
+                },
+                // {
+                //     test: /\.svg$/,
+                //     use: "@svgr/webpack",
+                // },
+            ]
+        );
+        return config;
+    },
 };
 
 module.exports = nextConfig;
