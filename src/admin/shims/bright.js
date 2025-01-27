ccaaac9ce9019135878b62e1ef4452b7e0fbc17c
Kyler Besher <kyler@Kylers-MBP.home>
2025-01-26 20:12:50 -0600

    update

--- src/admin/shims/bright.js
+++ src/admin/shims/bright.js
@@ -1,4 +1,15 @@
 // Minimal bright shim for client-side
+const Code = ({ code, language }) => {
+    return {
+        type: 'code',
+        props: {
+            code,
+            language,
+            html: `<pre><code class="language-${language}">${code}</code></pre>`
+        }
+    };
+};
+
 const bright = {
     highlight: (code, lang) => {
         return {
@@ -6,7 +17,9 @@ const bright = {
             lang,
             html: `<pre><code class="language-${lang}">${code}</code></pre>`
         };
-    }
+    },
+    Code
 };
 
+export { Code };
 export default bright;
