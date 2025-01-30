2761f3620a3b938c2fd1a78da3802a6ddec4409f
Kyler Besher <kyler@Kylers-MBP.home>
2025-01-29 23:32:12 -0600

    build locally

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -1,8 +1,8 @@
 import '../../styles/globals.css';
-import { Footer } from '../../../components/footer';
-import { Header } from '../../../components/header';
-import { ThemeProvider } from '../../../lib/theme/ThemeContext';
-import { DarkModeProvider } from '../../../lib/theme/DarkModeContext';
+import { Footer } from '../../components/footer';
+import { Header } from '../../components/header';
+import { ThemeProvider } from '../../lib/theme/ThemeContext';
+import { DarkModeProvider } from '../../lib/theme/DarkModeContext';
 import { StringProvider } from '../../lib/context/StringContext';
 import siteConfig from '../../content/settings/site.json';
 
